65d5dfd889cc989b661420fde41dd6aa7e69c78d
Phillip Kataswa <65820314+Artsia@users.noreply.github.com>
2023-03-29 22:03:57 -0400

    parse api data correctly

--- src/Comp/useDictionaryApi.js
+++ src/Comp/useDictionaryApi.js
@@ -2,18 +2,33 @@ import React from "react";
 import useFetch from "react-fetch-hook";
 
 
-export default function App({searching}) {
+export default function App({ searching }) {
   const { isLoading, error, data } = useFetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${searching}`);
 
   if (isLoading) return "Loading...";
-  if (error) return "Error!";
+  if (error) return "Enter a Word";
 
   return (
-    <div>
+    <div className="dicContainer">
       {data.map((result, index) => (
         <div key={index}>
           <h3>{result.word}</h3>
-          <p>{result.meanings[0].definitions[0].definition}</p>
+          <div className="meaningDiv">
+
+            <p>{result.meanings[0].partOfSpeech}</p> {/**Noun*/}
+
+            <p>Definition 1: {result.meanings[0].definitions[0].definition}</p>
+          </div>
+        </div>
+      ))}
+
+
+      {data.map((result, index) => (
+        <div>
+          <div className="meaningDiv">
+            <p>Example: {result.meanings[0].definitions[0].example}</p>
+          </div>
+          
         </div>
       ))}
     </div>
